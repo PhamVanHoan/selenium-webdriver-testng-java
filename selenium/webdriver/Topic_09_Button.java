12b490667ed044e3797b00af5cfb3b6705884658
Pham Van Hoan <hoan96nb@gmail.com>
2022-08-19 23:26:55 +0700

    Handle Button

--- selenium/webdriver/Topic_09_Button.java
+++ selenium/webdriver/Topic_09_Button.java
@@ -12,7 +12,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_00_Template {
+public class Topic_09_Button {
 
 	// Khai báo
 	WebDriver driver;
@@ -45,7 +45,42 @@ public class Topic_00_Template {
 	}
 
 	@Test
-	public void TC_01() {
+	public void TC_01_Fahasa() {
+		driver.get("https://www.fahasa.com/customer/account/create");
+		sleepInSecond(4);
+
+		/*
+		 * //Switch qua iframe trước khi Close popup (do popup nằm trong iframe)
+		 * driver.switchTo().frame("preview-notification-frame");
+		 * 
+		 * //Close popup
+		 * driver.findElement(By.cssSelector("a#NC_CLOSE_ICON>img")).click();
+		 * sleepInSecond(2);
+		 * 
+		 * // Quay về trang trước đó chứa iframe driver.switchTo().defaultContent();
+		 */
+
+		// Chuyển qua tab login
+		driver.findElement(By.cssSelector("li.popup-login-tab-login")).click();
+
+		// Verify Đăng nhập button is disable
+		Assert.assertFalse(driver.findElement(By.cssSelector("button.fhs-btn-login")).isEnabled());
+
+		// Enter value to Email and Password textbox
+		driver.findElement(By.cssSelector("input#login_username")).sendKeys("a@gmail.com");
+		driver.findElement(By.cssSelector("input#login_password")).sendKeys("123456");
+
+		// Verify button Đăng nhập is enable
+		Assert.assertTrue(driver.findElement(By.cssSelector("button.fhs-btn-login")).isEnabled());
+
+		// Verify background color của Đăng nhập button
+		String rgbaColor = driver.findElement(By.cssSelector("button.fhs-btn-login")).getCssValue("background-color");
+
+		// rgbaColor convert to Hexa color
+		String hexaColor = Color.fromString(rgbaColor).asHex().toUpperCase();
+
+		// Verify back-ground color
+		Assert.assertEquals(hexaColor, "#C92127");
 
 	}
 
