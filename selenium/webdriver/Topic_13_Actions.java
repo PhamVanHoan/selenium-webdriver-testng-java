585a2238638537a053725c572e3c9da7595a370d
Pham Van Hoan <hoan96nb@gmail.com>
2022-08-24 14:54:09 +0700

    Actions

---
+++ selenium/webdriver/Topic_13_Actions.java
@@ -0,0 +1,86 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_13_Actions {
+
+	// Khai báo
+	WebDriver driver;
+	WebDriverWait explicitWait;
+	Actions action;
+
+	// Khai báo + khởi tạo
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+
+	@BeforeClass
+	public void beforeClass() {
+		if (osName.contains("Mac")) {// Mac
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver.exe");
+
+		} else // Windows
+
+		{
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		}
+
+		// Khởi tạo driver
+		driver = new FirefoxDriver();
+
+		// Khởi tạo wait
+		explicitWait = new WebDriverWait(driver, 30);
+		
+		//Khởi tạo action
+		
+		action = new Actions(driver);
+
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+	}
+
+	@Test
+	public void TC_01_Hover() {
+		driver.get("https://automationfc.github.io/jquery-tooltip/");
+		
+		//Hover to element
+		action.moveToElement(driver.findElement(By.cssSelector("input#age"))).perform();
+		
+		Assert.assertEquals(driver.findElement(By.cssSelector("div.ui-tooltip-content")), "We ask for your age only for statistical purposes.");
+	}
+
+	@Test
+	public void TC_02() {
+	}
+
+	@Test
+	public void TC_03() {
+
+		sleepInSecond(5);
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	// Sleep cứng (Static wait)
+	public void sleepInSecond(long timeInSecond) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
