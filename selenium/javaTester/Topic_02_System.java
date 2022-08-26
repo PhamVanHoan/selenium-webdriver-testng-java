93bda335fa41fe87a18647041e6efd8216773dd3
Pham Van Hoan <hoan96nb@gmail.com>
2022-08-26 22:25:41 +0700

    System

---
+++ selenium/javaTester/Topic_02_System.java
@@ -0,0 +1,13 @@
+package javaTester;
+
+public class Topic_02_System {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String projectPath = System.getProperty("user.dir");
+		String osName = System.getProperty("os.name");
+		System.out.println(projectPath);
+		System.out.println(osName);
+	}
+
+}
